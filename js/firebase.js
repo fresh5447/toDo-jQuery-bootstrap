9bfad9a08d880697e80fa12e4cfb33d423dbebff
fresh5447 <doug@kosmojo.com>
2017-01-25 17:24:59 -0700

    add firebase config to file

---
+++ js/firebase.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "jquery-todo-662f8.firebaseapp.com",
+  databaseURL: "https://jquery-todo-662f8.firebaseio.com",
+  storageBucket: "jquery-todo-662f8.appspot.com",
+  messagingSenderId: "573512969580"
+};
+firebase.initializeApp(config);
